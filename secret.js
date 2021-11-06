525362746d6cee3219cb5173c8dc78fbc2b0d5c2
Masterprogrammerabe <92180502+Masterprogrammerabe@users.noreply.github.com>
2021-11-06 16:56:07 -0400

    Create secret.js

---
+++ secret.js
@@ -0,0 +1,8 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop()
+secretMessage.push('to','program')
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Pragramming')
+secretMessage.splice([6],[10], 'know,')
+console.log(secretMessage)
